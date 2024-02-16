a2c86e6bea5a505788d2b33328d087b94b6072c1
Gabriel Vargas Bravo <gabriel.vargas.bravo@gmail.com>
2024-02-16 14:13:14 -0500

    update: environments comments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,3 @@
 export const environment = {
   countriesApi: 'https://restcountries.com/v3.1',
 };
-
-// /all?fields=name,population,region,area,flags,independent,unMember,subregion
-
-// /name/peru?fullText=true&fields=name,population,region,area,flags,independent,unMember,subregion,languages,currencies,capital,borders
